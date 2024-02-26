a8399650db4e1f9e8d4c26a4facc6f47003f6e50
JayRGopal <jay2003@me.com>
2024-02-26 00:26:10 -0500

    feat: history and input for mode command debugged

--- mock/src/components/REPLInput.tsx
+++ mock/src/components/REPLInput.tsx
@@ -1,12 +1,14 @@
 import "../styles/main.css";
 import { Dispatch, SetStateAction, useState } from "react";
 import { ControlledInput } from "./ControlledInput";
+import { commandHub, getCommand } from "./CommandHub";
+import { outputMode } from "./commandMode";
 
 interface REPLInputProps {
   
-  history: string[];
-  setHistory: React.Dispatch<React.SetStateAction<string[]>>;
-  
+  history: { command: string; result: string }[];
+  setHistory: React.Dispatch<React.SetStateAction<{ command: string; result: string }[]>>;
+
 }
 
 
@@ -15,9 +17,23 @@ export function REPLInput(props: REPLInputProps) {
   const [commandString, setCommandString] = useState<string>("");
   const [count, setCount] = useState<number>(0);
 
+
   const handleSubmit = () => {
+    // Source: https://stackoverflow.com/questions/10346722/how-to-split-a-string-by-white-space-or-comma
+    const [coreCommand, ...args] = commandString.split(/\s+/);
+
+    const commandFunction = getCommand(coreCommand);
+
+    let result = '';
+    if (commandFunction) {
+      result = commandFunction(args);
+    } else {
+      result = `Command not found: ${coreCommand}`;
+    }
+
+    props.setHistory([...props.history, { command: commandString, result: result }]);
+    
     setCount(count + 1);
-    props.setHistory([...props.history, commandString]);
     setCommandString("");
   };
 
